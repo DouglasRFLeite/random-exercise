6ce009cf50a41658906ed50884a54dfc8b1738b5
DouglasRFLeite <douglas.rochedo@gmail.com>
2024-04-04 13:12:53 -0300

    Mudança para deploy

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 5000
+EXPOSE 8080
 
-CMD ["gunicorn"  , "-b", "0.0.0.0:5000", "app:app"]
+CMD ["gunicorn"  , "-b", "0.0.0.0:8080", "app:app"]
--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def go_exercise():
     return render_template('index.html')
 
+@app.route("/healthz")
+def healthz():
+    return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080)
