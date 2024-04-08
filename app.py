5e0097738cef67b1d4593c7276e333b4a1923119
DouglasRFLeite <douglas.rochedo@gmail.com>
2024-04-08 00:41:15 -0300

    Html acessa flask, flask acessa API

--- app.py
+++ app.py
@@ -1,31 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('loading.html')
+    return render_template('index.html')
 
-@app.route("/get_exercise_data")
+@app.route("/hello")
+def hello():
+    return "hello"
+
+@app.route("/api/get_exercise_data")
 def get_exercise_data():
+    exercicio_aleatorio = random.randint(0, 1299)
     try:
-        # Faça a solicitação para a API externa usando a chave de API protegida
-        url = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/cardio?limit=10'
-        headers = {'X-RapidAPI-Key': 'SuaChaveDeAPIAqui'}
-        response = requests.get(url, headers=headers)
+        url = "https://exercisedb.p.rapidapi.com/exercises"
+        querystring = {"limit":"1","offset":exercicio_aleatorio}
+        headers = {
+            "X-RapidAPI-Key": "1a6191428bmshd5ace4b8d86cad3p178e0ejsnb39eb0718b4b",
+            "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
+        }
+        response = requests.get(url, headers=headers, params=querystring)
 
         # Processa a resposta conforme necessário
         if response.status_code == 200:
             data = response.json()
-            # Renderiza o template com os dados recebidos da API
-            return render_template('index.html', data=data)
+            # Retorna os dados da API em formato JSON
+            return jsonify(data)
         else:
             # Retorna uma mensagem de erro em caso de falha na solicitação
-            return render_template('error.html', message='Erro ao acessar a API externa')
+            return jsonify({'error': 'Erro ao acessar a API externa'}), 500
     except Exception as e:
         # Retorna uma mensagem de erro em caso de exceção
-        return render_template('error.html', message=str(e))
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,53 +1,63 @@
 function capitalizeFirstLetterOfEachWord(string) {
-    // Dividir a string em palavras usando espaço como delimitador
-    const words = string.split(" ");
+  // Dividir a string em palavras usando espaço como delimitador
+  const words = string.split(" ");
 
-    // Capitalizar a primeira letra de cada palavra
-    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  // Capitalizar a primeira letra de cada palavra
+  const capitalizedWords = words.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1)
+  );
 
-    // Juntar as palavras novamente em uma string
-    const capitalizedString = capitalizedWords.join(" ");
+  // Juntar as palavras novamente em uma string
+  const capitalizedString = capitalizedWords.join(" ");
 
-    return capitalizedString;
+  return capitalizedString;
 }
 
-const url = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/cardio?limit=10';
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
+const baseUrl = window.location.href +"/api/get_exercise_data";
+
+const fetchExerciseData = () => {
+  try {
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        data = data[0];
+        document.getElementById("exercise-image").src =
+          data.gifUrl;
+        // Set other exercise information
+        document.getElementById("exercise-name").innerText =
+          capitalizeFirstLetterOfEachWord(data.name);
+        document.getElementById("exercise-body-part").innerText =
+          capitalizeFirstLetterOfEachWord(data.bodyPart);
+        document.getElementById("exercise-equipment").innerText =
+          capitalizeFirstLetterOfEachWord(data.equipment);
+        document.getElementById("exercise-target").innerText =
+          capitalizeFirstLetterOfEachWord(data.target);
+        const badgeSection = document.getElementById("badge-list");
+        data.secondaryMuscles.forEach((muscle) => {
+          const badge = document.createElement("span");
+          badge.classList.add(
+            "badge",
+            "bg-danger-subtle",
+            "text-danger-emphasis"
+          );
+          badge.innerText = capitalizeFirstLetterOfEachWord(muscle);
+          badgeSection.appendChild(badge);
+        });
+        const instructionsList = document.getElementById(
+          "exercise-instructions"
+        );
+        data.instructions.forEach((instruction) => {
+          const instructionListItem = document.createElement("li");
+          instructionListItem.innerText = instruction;
+          instructionsList.appendChild(instructionListItem);
+          console.log(instruction);
+        });
+        
+      });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-try {
-    fetch(url, options)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            // Choose image
-            const randomExercise = Math.floor(Math.random() * 10)
-            // Set exercise image
-            document.getElementById('exercise-image').src = data[randomExercise].gifUrl;
-            // Set other exercise information
-            document.getElementById('exercise-name').innerText = capitalizeFirstLetterOfEachWord(data[randomExercise].name);
-            document.getElementById('exercise-body-part').innerText = capitalizeFirstLetterOfEachWord(data[randomExercise].bodyPart);
-            document.getElementById('exercise-equipment').innerText = capitalizeFirstLetterOfEachWord(data[randomExercise].equipment);
-            document.getElementById('exercise-target').innerText = capitalizeFirstLetterOfEachWord(data[randomExercise].target);
-            const badgeSection = document.getElementById('badge-list');
-            data[randomExercise].secondaryMuscles.forEach(muscle => {
-                const badge = document.createElement('span');
-                badge.classList.add('badge', 'bg-danger-subtle', 'text-danger-emphasis')
-                badge.innerText = capitalizeFirstLetterOfEachWord(muscle);;
-                badgeSection.appendChild(badge);
-            });
-            const instructionsList = document.getElementById('exercise-instructions');
-            data[randomExercise].instructions.forEach(instruction => {
-                const instructionListItem = document.createElement('li');
-                instructionListItem.innerText = instruction;
-                instructionsList.appendChild(instructionListItem);
-            });
-        })
-} catch (error) {
-    console.error(error);
-}
+fetchExerciseData();
